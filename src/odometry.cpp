4e06010a5ed0f4246cfbee90dd998827e8086241
Raushan Kumar <rashukumar1504@gmail.com>
2025-08-28 19:35:28 +0530

    Update odometry.cpp

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 Odometry::Odometry(double wheel_radius, double rpm)
     : radius(wheel_radius), rpm(rpm) {
-  // Linear velocity (m/s) =(wheel circumference * revolutions per second)
+  
   double rps = rpm / 60.0;
   linear_vel = 2 * M_PI * radius * rps;
 }
@@ -25,8 +25,27 @@ double Odometry::angle(int x1, int y1, int x2, int y2) {
 MotionCommand Odometry::computeCommands(vector<pair<int, int>> &path) {
 
   MotionCommand res = {0.0, 0.0}; // store total time and angle traversed
+if (path.size() < 2) return res; 
+    double prev_angle = angle(path[0].first, path[0].second,
+                              path[1].first, path[1].second);
 
- /* Implement you odometry logic here */ 
+    for (size_t i = 1; i < path.size(); i++) {
+        int x1 = path[i-1].first, y1 = path[i-1].second;
+        int x2 = path[i].first, y2 = path[i].second;
+        double d = distance(x1, y1, x2, y2);
+        double travel_time = d / linear_vel; // time = distance / velocity
+        res.total_time += travel_time;
+        double curr_angle = angle(x1, y1, x2, y2);
+        double turn = curr_angle - prev_angle;
+        while (turn > 180) turn -= 360;
+        while (turn < -180) turn += 360;
+
+        res.total_angle += fabs(turn); // accumulate absolute turns
+        prev_angle = curr_angle;
+    }
+
+    
 
   return res;
 }
+//could not compile the code in gcc/g++ compiler
